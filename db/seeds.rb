8a52a0ecaa8064516275579a63fd09bc7c880c1a
Salem Amba <salemamba@gmail.com>
2016-03-02 23:21:21 -0800

    added rails-bower gem and installed angularjs components

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+r = Random.new 
+50.times do |n|
+	title = "todo#{n}"
+	completed = [false, true][r.rand(2)]
+	Todo.create!(title: title, completed: completed)
+end
